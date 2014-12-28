f2921b83ade241014f41f51d927a23482a24bd4c
Akinori Maesoko <s13012@std.it-college.ac.jp>
2014-12-29 00:28:09 +0900

    add: project file

---
+++ Assets/LeapMotion/Scripts/Hands/HandChecker.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class HandChecker : MonoBehaviour {
+
+	private GUIText text;
+	private HandController controller;
+
+	// Use this for initialization
+	void Start () {
+		text = gameObject.GetComponent (typeof(GUIText)) as GUIText;
+		controller = (GameObject.Find("HandController") as GameObject)
+			.GetComponent(typeof(HandController)) as HandController;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = handCheck(controller.ExtendedFingers);
+	}
+
+	private string handCheck(int fingers) {
+		string hand = "";
+		switch(fingers) {
+			case 0: hand = "グー"; break;
+			case 2: hand = "チョキ"; break;
+			case 5: hand = "パー"; break;
+			default: hand = ""; break;
+		}
+		return hand;
+	}
+}
